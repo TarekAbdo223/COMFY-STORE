51df1c29057b36e0d3296696c006e1349767b91a
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-08-20 17:29:28 +0300

    pages setup

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    [require("@tailwindcss/typography"), require("daisyui")],
     tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] }),
