294f38e772b8ee78c62ca47c5f93708d61797f26
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-08-22 15:25:29 +0300

    added SubmitButton spinner

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,9 +1,25 @@
-const submitBtn = ({ text }) => {
+import { useNavigation } from "react-router-dom";
+
+const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
-    <div type="submit" className="btn btn-primary">
-      {text}
-    </div>
+    <button
+      type="submit"
+      className="btn btn-primary btn-block"
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner"></span>
+          sending...
+        </>
+      ) : (
+        text || "submit"
+      )}
+    </button>
   );
 };
 
-export default submitBtn;
+export default SubmitBtn;
