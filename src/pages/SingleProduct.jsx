ee6360e23c6f5266ad71f4c20aae5ce7c79ae24d
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-09-05 14:39:50 +0300

    added the colors

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { customFetch, formatPrice } from "../utils";
 import { Link, useLoaderData } from "react-router-dom";
 
@@ -16,6 +16,7 @@ const SingleProduct = () => {
   const { image, title, price, description, colors, company } =
     product.attributes;
   const dollarsAmount = formatPrice(price);
+  const [productColor, setProductColor] = useState(colors[0]);
 
   return (
     <section>
@@ -45,6 +46,25 @@ const SingleProduct = () => {
           </h4>
           <p className="mt-3 text-xl">{dollarsAmount}</p>
           <p className="mt-6 leading-8">{description}</p>
+          {/* colors */}
+          <div className="mt-6">
+            <h4 className="text-md font-medium tracking-wider capitalize">
+              colors
+            </h4>
+            <div className="mt-2">
+              {colors.map((color) => {
+                return (
+                  <button
+                    key={color}
+                    type="button"
+                    className={`badge w-6 h-6 mr-2 ${color === productColor && "border-2 border-secondary"}`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
