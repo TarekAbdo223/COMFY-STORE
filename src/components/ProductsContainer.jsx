52b855382e2b39f4249e9b532efc18e4044e7aa5
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-09-12 15:44:23 +0300

    added the list view for the products on the products page

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
 
 const ProductsContainer = () => {
-  return <div>ProductsContainer</div>;
+  return (
+    <>
+      <ProductsGrid />
+      <ProductsList />
+    </>
+  );
 };
 
 export default ProductsContainer;
