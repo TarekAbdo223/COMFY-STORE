a8a1f65dca18f36df51a1500b987ae6a4351a288
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-09-23 01:49:25 +0300

    finish the pagination contianer and the next - prev buttons

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -24,7 +24,11 @@ const PaginationContainer = () => {
       <div className="join">
         <button
           className="btn btn-xs sm:btn-md join-item"
-          onClick={() => handlePageChange("prev")}
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
+          }}
         >
           Prev
         </button>
@@ -41,7 +45,11 @@ const PaginationContainer = () => {
         })}
         <button
           className="btn btn-xs sm:btn-md join-item"
-          onClick={() => handlePageChange("next")}
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) nextPage = 1;
+            handlePageChange(nextPage);
+          }}
         >
           Next
         </button>
